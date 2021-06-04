5ebbc91b102b2ec41fda1a2b20669299801e6a5f
Steve <stevebarakat@gmail.com>
2021-06-04 19:10:03 -0400

    fixed midpoint starting value

---
+++ src/RangeSlider.js
@@ -0,0 +1,155 @@
+import React, { useState, useEffect, useRef } from "react";
+import styled from 'styled-components';
+
+let fillColor = "";
+let newValue = "";
+
+const RangeSlider = ({ min = 0, max = 100, step = 0, decimals = 0, primaryColor = "black" }) => {
+  const rangeEl = useRef(null);
+  const [value, setValue] = useState((min + max) / 2);
+  const [isFocused, setIsFocused] = useState(false);
+  newValue = Number(((value - min) * 100) / (max - min));
+  const newPosition = 10 - newValue * 0.2;
+  fillColor = primaryColor;
+
+  useEffect(() => {
+    rangeEl.current.focus();
+    setValue(rangeEl.current.value)
+  }, [])
+
+  return (
+    <RangeWrap>
+      <RangeOutput
+        focused={isFocused}
+        style={{ left: `calc(${newValue}% + (${newPosition / 10}rem))` }}
+        className="range-value"
+      >
+        {parseFloat(value).toFixed(decimals)}
+      </RangeOutput>
+      <StyledRangeSlider
+        min={min}
+        max={max}
+        step={step}
+        value={value}
+        onInput={(e) => {
+          rangeEl.current.focus();
+          setValue(e.target.value);
+        }}
+        ref={rangeEl}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
+      <Progress
+        focused={isFocused}
+        style={{ width: `calc(${newValue}% + (${newPosition / 10}rem))` }}
+        // style={{width: min - max / 2 }}
+      />
+    </RangeWrap>
+  );
+};
+
+export default RangeSlider;
+
+const whiteColor = "white";
+const blackColor = "#999";
+
+const StyledRangeSlider = styled.input.attrs({ type: "range" })`
+  appearance: none;
+  margin: 20px 0;
+  width: 100%;
+  &:focus {
+    outline: none;
+  }
+  &::-webkit-slider-thumb {
+    position: relative;
+    height: 2.2rem;
+    width: 2.2rem;
+    border-radius: 50%;
+    background: ${whiteColor};
+    box-shadow: 0 0 4px 0 rgba(0, 0, 0, 1);
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: -10px;
+    z-index: 999;
+  }
+  &::-moz-range-thumb {
+    position: relative;
+    height: 2.2rem;
+    width: 2.2rem;
+    border-radius: 50%;
+    background: ${whiteColor};
+    box-shadow: 0 0 4px 0 rgba(0, 0, 0, 1);
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: -10px;
+    z-index: 999;
+  }
+  &::-webkit-slider-runnable-track {
+    width: 15px;
+    height: 15px;
+    cursor: pointer;
+    background: white;
+    border-radius: 25px;
+    box-shadow: inset 0 0 3px 1px rgba(0, 0, 0, 0.5);
+  }
+  &::-moz-range-runnable-track {
+    width: 15px;
+    height: 15px;
+    cursor: pointer;
+    background: white;
+    border-radius: 25px;
+    box-shadow: inset 0 0 3px 1px rgba(0, 0, 0, 0.5);
+  }
+  &:focus::-webkit-slider-runnable-track{
+    background: whiteColor;
+  }
+  &:focus::-moz-range-runnable-track{
+    background: whiteColor;
+  }
+
+  &:focus::-webkit-slider-thumb {
+    background: ${p => p.focused ? whiteColor : `-webkit-radial-gradient(center, ellipse cover,  ${fillColor} 0%,${fillColor} 35%,${whiteColor} 40%,${whiteColor} 100%)`};
+    transition: all 0.15s ease-out;
+  }
+  &:focus::-moz-range-thumb {
+    background: ${p => p.focused ? whiteColor : `-webkit-radial-gradient(center, ellipse cover,  ${fillColor} 0%,${fillColor} 35%,${whiteColor} 40%,${whiteColor} 100%)`};
+    transition: all 0.15s ease-out;
+  }
+`;
+
+const RangeWrap = styled.div`
+  border: 3px dotted red;
+  position: relative;
+  margin-top: 2rem;
+`;
+
+const Progress = styled.div`
+  background: ${p => p.focused ? fillColor : "#EFEFEF"};
+  height: 15px;
+  width: 15px;
+  border-radius: 25px;
+  position: absolute;
+  top: 20px;
+  box-shadow: inset 0 0 3px 1px rgba(0, 0, 0, 0.5);
+  z-index: 0;
+  transition: all 0.15s ease-out;
+`;
+
+const RangeOutput = styled.div`
+  font-family: sans-serif;
+  position: absolute;
+  margin-top: -2rem;
+  left: 50%;
+  border: ${p => p.focused ? "none" : `1px solid ${blackColor}`};
+  background: ${p => p.focused ? fillColor : whiteColor};
+  color: ${p => p.focused ? whiteColor : blackColor};
+  line-height: 1.75rem;
+  text-align: center;
+  padding: 0.15rem 0.5rem;
+  font-size: 1rem;
+  display: block;
+  transform: translate(-50%, 0);
+  border-radius: 5px;
+  box-shadow: 0 1px 5px 0 rgba(0, 0, 0, 0.25);
+  transition: all 0.15s ease-out;
+`;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import RangeSlider from './RangeSlider';
+import './global.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RangeSlider min={23} max={100} step={0} decimals={0} primaryColor="hsl(196, 100%, 48%)" />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
